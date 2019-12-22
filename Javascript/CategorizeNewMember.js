807d7e9a1f848528f0bec0448df6b2bb188b18e8
Yael Brown <yaelrbrown@gmail.com>
2019-12-21 23:26:55 -0600

    Added js solutions

---
+++ Javascript/CategorizeNewMember.js
@@ -0,0 +1,22 @@
+/*
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input
+Input will consist of a list of lists containing two items each. Each list contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Note for F#: The input will be of (int list list) which is a List<List>
+*/
+
+function openOrSenior(data) {
+  let out = [];
+  data.map((e,i) => {
+    if (e[0] >= 55 && e[1] > 7) {
+      out.push("Senior");
+    } else {
+      out.push("Open");
+    }
+  });
+  return out;
+}
